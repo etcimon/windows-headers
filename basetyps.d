217e0a298197484afdee0d9b22cef4cbcb8de9b1
Don Clugston <Don Clugston@e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd>
2006-04-24 11:42:13 +0000

    Added basetyps. There's not much to it.
    git-svn-id: http://svn.dsource.org/projects/bindings/trunk/win32@76 e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd

---
+++ basetyps.d
@@ -0,0 +1,25 @@
+/* Converted from MinGW w32api/basetyps.h
+   Public domain.
+*/
+module win32.basetyps;
+private import win32.windef;
+
+struct GUID {          // size is 16
+    align(1):
+	DWORD Data1;
+	WORD  Data2;
+	WORD  Data3;
+	BYTE  Data4[8];
+}
+alias GUID * REFGUID, LPGUID;
+alias GUID UUID;
+alias GUID IID;
+alias GUID CLSID;
+
+alias CLSID * LPCLSID, REFCLSID;
+alias IID * LPIID, REFIID;
+alias GUID FMTID;
+alias FMTID * REFFMTID;
+alias uint error_status_t;
+alias UUID uuid_t;
+alias uint PROPID;
