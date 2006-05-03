97d0ae7954fc2f0638cc6d38b09e447b89ac3620
Don Clugston <Don Clugston@e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd>
2006-05-03 15:28:54 +0000

    Added objbase.d
    git-svn-id: http://svn.dsource.org/projects/bindings/trunk/win32@95 e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd

--- objidl.d
+++ objidl.d
@@ -2,3 +2,10 @@ module win32.objidl;
 
 import win32.objfwd;
 pragma(msg, "objidl.d : Not yet converted!");
+
+// So that objbase will compile.
+struct MULTI_QI {}
+struct SNB {}
+struct BIND_OPTS {}
+struct SOLE_AUTHENTICATION_SERVICE {}
+struct  COSERVERINFO {}
