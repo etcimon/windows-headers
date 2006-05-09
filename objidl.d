57af350291165ec0154fc8efcc1beed516e189f6
Don Clugston <Don Clugston@e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd>
2006-05-09 15:30:49 +0000

    Added ole2.
    git-svn-id: http://svn.dsource.org/projects/bindings/trunk/win32@105 e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd

--- objidl.d
+++ objidl.d
@@ -9,3 +9,21 @@ struct SNB {}
 struct BIND_OPTS {}
 struct SOLE_AUTHENTICATION_SERVICE {}
 struct  COSERVERINFO {}
+
+// so that ole2 will compile.
+alias short CLIPFORMAT;
+alias void * LPFORMATETC;
+alias void * LPOLECLIENTSITE;
+alias void * LPDROPTARGET;
+alias void * LPOLEOBJECT;
+alias void * LPOLEADVISEHOLDER;
+alias int HOLEMENU;
+alias void * LPDROPSOURCE;
+alias void * LPOLEMENUGROUPWIDTHS;
+alias void * LPMSG;
+alias void * LPENUMOLEVERB;
+alias void * LPSTGMEDIUM;
+alias void * LPOLEINPLACEFRAME;
+alias void * LPOLEINPLACEACTIVEOBJECT;
+alias void * LPOLEINPLACEFRAMEINFO;
+struct DVTARGETDEVICE {}
