4c3b799a0f7410306c03b5678b2d8f8665c8d0c3
Don Clugston <Don Clugston@e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd>
2006-07-19 19:28:14 +0000

    Bugfix to allow it external projects to compile with -cov (now has version = WindowsUnitTest). Included some files that were left off before.
    git-svn-id: http://svn.dsource.org/projects/bindings/trunk/win32@147 e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd

---
+++ nddeapi.d
@@ -0,0 +1,12 @@
+/***********************************************************************\
+*                               nddeapi.d                               *
+*                                                                       *
+*                       Windows API header module                       *
+*                                                                       *
+*                 Translated from MinGW Windows headers                 *
+*                           by Stewart Gordon                           *
+*                                                                       *
+*                       Placed into public domain                       *
+\***********************************************************************/
+module win32.nddeapi;
+version { }
