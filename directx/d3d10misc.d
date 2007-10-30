739991e06611930ce3b0eb98bda7c8de485637de
WeirdCat <WeirdCat@e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd>
2007-10-30 10:02:35 +0000

    d3d10.d should now be complete
    git-svn-id: http://svn.dsource.org/projects/bindings/trunk/win32@224 e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd

--- directx/d3d10misc.d
+++ directx/d3d10misc.d
@@ -30,27 +30,10 @@ enum D3D10_DRIVER_TYPE {
 extern (C) const GUID GUID_DeviceType = {0xd722fb4d, 0x7a68, 0x437a, [0xb2, 0x0c, 0x58, 0x04, 0xee, 0x24, 0x94, 0xa6]};
 
 extern(Windows) {
-
-	HRESULT D3D10CreateDevice(
-		IDXGIAdapter pAdapter,
-		D3D10_DRIVER_TYPE DriverType,
-		HMODULE Software,
-		UINT Flags,
-		UINT SDKVersion,
-		ID3D10Device* ppDevice);
-
-	HRESULT D3D10CreateDeviceAndSwapChain(
-		IDXGIAdapter pAdapter,
-		D3D10_DRIVER_TYPE DriverType,
-		HMODULE Software,
-		UINT Flags,
-		UINT SDKVersion,
-		DXGI_SWAP_CHAIN_DESC* pSwapChainDesc,
-		IDXGISwapChain* ppSwapChain,    
-		ID3D10Device* ppDevice);
-
-	HRESULT D3D10CreateBlob(
-		SIZE_T NumBytes,
-		ID3D10Blob* ppBuffer);
-	
+	HRESULT D3D10CreateDevice(IDXGIAdapter pAdapter, D3D10_DRIVER_TYPE DriverType,
+		HMODULE Software, UINT Flags, UINT SDKVersion, ID3D10Device* ppDevice);
+	HRESULT D3D10CreateDeviceAndSwapChain(IDXGIAdapter pAdapter,
+		D3D10_DRIVER_TYPE DriverType, HMODULE Software, UINT Flags, UINT SDKVersion,
+		DXGI_SWAP_CHAIN_DESC* pSwapChainDesc, IDXGISwapChain* ppSwapChain, ID3D10Device* ppDevice);
+	HRESULT D3D10CreateBlob(SIZE_T NumBytes, ID3D10Blob* ppBuffer);
 }
