a30557b63d01a979711b0b37aa01c4488a631921
SnakE <SnakE@e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd>
2009-02-23 14:25:19 +0000

    Fixed calling convention for PSAPI
    The PSAPI functions actually use Windows calling convention, not C.
    
    
    git-svn-id: http://svn.dsource.org/projects/bindings/trunk/win32@325 e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd

--- psapi.d
+++ psapi.d
@@ -90,7 +90,7 @@ version (Unicode) {
 }
 
 // Grouped by application, not in alphabetical order.
-extern (C) {
+extern (Windows) {
 	/* Process Information
 	 * http://windowssdk.msdn.microsoft.com/library/ms684870.aspx */
 	BOOL EnumProcesses(DWORD*, DWORD, DWORD*); /* NT/2000/XP/Server2003/Vista/Longhorn */
