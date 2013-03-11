f5d423dce877af4af5f3c8dec9651693a0ffd411
CyberShadow <CyberShadow@e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd>
2013-03-11 06:02:45 +0000

    win32.rapi: Use native D interfaces instead of Vtbl structs
    Since D interfaces are reference types, this module was an exception in that it defined an IRapiStream type which was a value type. This required using "IRapiStream*" in places where normally one would write "IRapiStream".
    
    This is a breaking change, although I doubt anyone actually uses this module (it's related to Windows CE).
    
    git-svn-id: http://svn.dsource.org/projects/bindings/trunk/win32@407 e3080b08-b6e0-0310-b0a6-cbd5e3aa05fd

--- rapi.d
+++ rapi.d
@@ -11,51 +11,44 @@
 module win32.rapi;
 
 /* Comment from MinGW
-	NOTE: This strictly does not belong in the Win32 API since it's
-	really part of Platform SDK.
+   NOTE: This strictly does not belong in the Win32 API since it's
+   really part of Platform SDK.
  */
 
 private import win32.winbase, win32.windef;
 
-struct IRAPIStream {
-	IRAPIStreamVtbl* lpVtbl;
+extern (Windows):
+
+enum RAPISTREAMFLAG
+{
+    STREAM_TIMEOUT_READ
 }
 
-enum RAPISTREAMFLAG {
-	STREAM_TIMEOUT_READ
+interface IRAPIStream
+{
+    HRESULT SetRapiStat(RAPISTREAMFLAG, DWORD);
+    HRESULT GetRapiStat(RAPISTREAMFLAG, DWORD*);
 }
 
-extern (Windows) {
-	alias HRESULT function(IRAPIStream*, RAPISTREAMFLAG, DWORD)  _SetRapiStat;
-	alias HRESULT function(IRAPIStream*, RAPISTREAMFLAG, DWORD*) _GetRapiStat;
+alias HRESULT function(DWORD, BYTE, DWORD, BYTE, IRAPIStream) RAPIEXT;
+
+struct RAPIINIT
+{
+    DWORD   cbSize = this.sizeof;
+    HANDLE  heRapiInit;
+    HRESULT hrRapiInit;
 }
 
-struct IRAPIStreamVtbl {
-	_SetRapiStat SetRapiStat;
-	_GetRapiStat GetRapiStat;
-}
-
-// FIXME: what's this?
-//typedef HRESULT(STDAPICALLTYPE RAPIEXT)(DWORD, BYTE, DWORD, BYTE, IRAPIStream*);
-
-struct RAPIINIT {
-	DWORD   cbSize = this.sizeof;
-	HANDLE  heRapiInit;
-	HRESULT hrRapiInit;
-}
-
-extern (Windows) {
-	HRESULT CeRapiInit();
-	HRESULT CeRapiInitEx(RAPIINIT*);
-	BOOL CeCreateProcess(LPCWSTR, LPCWSTR, LPSECURITY_ATTRIBUTES,
-	  LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPWSTR, LPSTARTUPINFO,
-	  LPPROCESS_INFORMATION);
-	HRESULT CeRapiUninit();
-	BOOL CeWriteFile(HANDLE, LPCVOID, DWORD, LPDWORD, LPOVERLAPPED);
-	HANDLE CeCreateFile(LPCWSTR, DWORD, DWORD, LPSECURITY_ATTRIBUTES, DWORD,
-	  DWORD, HANDLE);
-	BOOL CeCreateDirectory(LPCWSTR, LPSECURITY_ATTRIBUTES);
-	DWORD CeGetLastError();
-	BOOL CeGetFileTime(HANDLE, LPFILETIME, LPFILETIME, LPFILETIME);
-	BOOL CeCloseHandle(HANDLE);
-}
+HRESULT CeRapiInit();
+HRESULT CeRapiInitEx(RAPIINIT*);
+BOOL CeCreateProcess(LPCWSTR, LPCWSTR, LPSECURITY_ATTRIBUTES,
+  LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPWSTR, LPSTARTUPINFO,
+  LPPROCESS_INFORMATION);
+HRESULT CeRapiUninit();
+BOOL CeWriteFile(HANDLE, LPCVOID, DWORD, LPDWORD, LPOVERLAPPED);
+HANDLE CeCreateFile(LPCWSTR, DWORD, DWORD, LPSECURITY_ATTRIBUTES, DWORD,
+  DWORD, HANDLE);
+BOOL CeCreateDirectory(LPCWSTR, LPSECURITY_ATTRIBUTES);
+DWORD CeGetLastError();
+BOOL CeGetFileTime(HANDLE, LPFILETIME, LPFILETIME, LPFILETIME);
+BOOL CeCloseHandle(HANDLE);
